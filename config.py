19ecf9ac91deb298b93d908f25b5946709e3d34f
eaglehawkinator <a.coates.8537@gmail.com>
2024-08-08 11:26:34 -0700

    added shebang for clarity of running cronjob

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ SECRET_KEY = 'gecnDZhBMzBeyX2VskQzbIHqKXveVXb3dbhBwFSq'
 
 ALPACA_EMERGENCY_CODE = '9c24f182-b276-4fc1-b51a-62d0509eda0d'
 
+ACTIVE = True
+
 PICKLE_PATH = os.path.join('pickles', 'order_log')
